efd7077b6c862386dabd9fdc343c4b93f443f9cc
gstroudharris <gstroudharris@gmail.com>
2025-08-22 20:25:51 -0700

    More work done on current build queue

--- Code/FooterItems/currently_building_item.gd
+++ Code/FooterItems/currently_building_item.gd
@@ -1,8 +1,19 @@
 extends Node
 
 var currently_building_item: Resource = null
-@onready var text_display = get_node("%Timer")
+var item_queued: bool = false
+@onready var text_display = get_node("%BuildingTitle")
+@onready var icon_display = get_node("%QueueIcon")
+@onready var queue_bar = get_node("%QueueProgress")
 
 func _process(delta: float) -> void:
 	if get_tree().root.get_node("Main").item_in_building_queue != null:
-		print("time to build!")
+		currently_building_item = get_tree().root.get_node("Main").item_in_building_queue
+		text_display.clear()
+		text_display.add_text(str(currently_building_item.item_display_name))
+		replace_texture(currently_building_item.item_image)
+		
+		item_queued = true
+
+func replace_texture(input_texture):
+	icon_display.texture = input_texture
